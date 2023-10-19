e060cf72a82e32b0f3a2f27b3bea47a2569d195d
Gautam-7252 <gautam7252@gmail.com>
2023-10-19 14:24:11 +0530

    update

--- Connect_DB/main.go
+++ Connect_DB/main.go
@@ -4,11 +4,13 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	pswd := os.Getenv("MYSQL_PASSWORD")
-	db, err := sql.Open("mysql", "root:"+pswd+"@tcp(localhost:3306)/test2")
+	db, err := sql.Open("mysql", "root:"+pswd+"@(127.0.0.1:3306)/test2")
 	if err != nil {
 		fmt.Println("Error validating SQL.Open arguements")
 		panic(err.Error())
@@ -20,7 +22,7 @@ func main() {
 		fmt.Println("error verifying connection with db.Ping")
 		panic(err.Error())
 	}
-	insert, err := db.Query("INSERT INTO `test2`.`customer`(`idcustomer`,`firstname`,`lastname`) VALUES ('7','shawn','marsh');")
+	insert, err := db.Query("INSERT INTO `test2`.`customer`(`idcustomer`,`firstname`,`lastname`) VALUES ('6','shawn','marsh');")
 	if err != nil {
 		panic(err.Error())
 	}
