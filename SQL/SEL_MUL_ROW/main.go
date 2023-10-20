e19fece9478ed8410a9ddaea688c934802e3f620
Gautam-7252 <gautam7252@gmail.com>
2023-10-20 11:17:42 +0530

    Select multiple rows

--- SQL/SEL_MUL_ROW/main.go
+++ SQL/SEL_MUL_ROW/main.go
@@ -6,13 +6,16 @@ import (
 	"html/template"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-var Employee struct {
+type Employee struct {
 	Id        int
 	Firstname string
-	lastname  string
+	Lastname  string
 }
+
 var db *sql.DB
 var tpl *template.Template
 
@@ -29,6 +32,7 @@ func main() {
 		fmt.Println("Error connecting Database")
 		panic(err)
 	}
+	defer db.Close()
 	err = db.Ping()
 	if err != nil {
 		fmt.Println("Error in db.Ping")
@@ -41,5 +45,26 @@ func main() {
 func search(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		tpl.ExecuteTemplate(w, "index2.html", nil)
+		return
 	}
+	r.ParseForm()
+	MinId := r.FormValue("MinId")
+	MaxId := r.FormValue("MaxId")
+	stmt := "SELECT * FROM customer WHERE idcustomer <= ? AND idcustomer >= ?;"
+	row, err := db.Query(stmt, MaxId, MinId)
+	if err != nil {
+		fmt.Println("Error in Query")
+		panic(err)
+	}
+	defer row.Close()
+	var Emp []Employee
+	for row.Next() {
+		var E Employee
+		err = row.Scan(&E.Id, &E.Firstname, &E.Lastname)
+		if err != nil {
+			panic(err)
+		}
+		Emp = append(Emp, E)
+	}
+	tpl.ExecuteTemplate(w, "index2.html", Emp)
 }
